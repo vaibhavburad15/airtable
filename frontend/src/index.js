149e00dc96e9fb6d3fd667dbecbd06d0c4d0b36a
vaibhavburad15 <94794397+vaibhavburad15@users.noreply.github.com>
2025-12-07 15:20:50 +0530

    Reorder import statements in index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import App from "./App";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter basename="/airtable">
